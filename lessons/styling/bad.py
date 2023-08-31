fcea0284a1c872ae9146369a143bacbd1f5c1e54
Jeevan Shah <jedimaster3284@gmail.com>
2023-08-31 19:17:31 -0400

    update bad.py

--- lessons/styling/bad.py
+++ lessons/styling/bad.py
@@ -3,12 +3,6 @@ from time import sleep
 
 
 
-
-
-
-
-
-
 def shape1(functioninput):
     why_do_i_have_to_wake_up_at_6 = 60
     for i_need_to_study_for_my_math_quiz in range(3):
@@ -17,61 +11,21 @@ def shape1(functioninput):
 def otHeRsHAPe(othershapefunctioninput):
     forward        (         othershapefunctioninput)
     right(120)
-
-
-
-
     forward(othershapefunctioninput)
     right(120)
     forward(othershapefunctioninput)
     right(120)
-
-
-
-
-
-
-
-
 
     right(60)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-
-    right (120         )
-    right(60)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    right( 60)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(             othershapefunctioninput                 )
-    right(120)
-    right(60)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    right(60)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    forward(othershapefunctioninput)
-    right(120)
-    right(60)
-    sleep(10000000000)
+    for variable in range(5):
+      forward(othershapefunctioninput)
+      right(120)
+      forward(othershapefunctioninput)
+      right(120)
+      forward(othershapefunctioninput)
+      right(120)
+      right( 60)
+    sleep(10000) 
 def     check(apple,banana ) :
     if int(apple ) ==banana:
         print(apple+"is equal to"+banana)
